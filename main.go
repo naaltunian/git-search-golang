6a08a138e22dbe5ef72472f9bf99ae6bcdb20e66
naaltunian <naaltunian@icloud.com>
2019-04-06 16:07:21 -0400

    Refactor

--- main.go
+++ main.go
@@ -1,18 +1,16 @@
 package main
 
 import (
-	"bufio"
 	"fmt"
-	"os"
 	// "encoding/json"
 )
 
 func main() {
 
 	// url := "https://api.github.com/users/"
-	reader := bufio.NewReader(os.Stdin)
+	var userName string
 
 	fmt.Println("Enter a github username:")
-	text, _ := reader.ReadString('\n')
-	fmt.Println(text)
+	fmt.Scanln(&userName)
+	fmt.Printf("Username: %s\n", userName)
 }
